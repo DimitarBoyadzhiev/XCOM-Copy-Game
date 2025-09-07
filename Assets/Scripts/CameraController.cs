25b932b34398a9e95d76e9f18afd7a2c4c6f6ad3
RavenTheNecromancer <dimitar.boyadzhiev02@gmail.com>
2025-09-07 13:40:40 +0300

    feat: grid system and camera control
    Implemented grid system keeping track of units. Implemented camera control system.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 10f;
+
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+
+    private CinemachineFollow cinemachineFollow;
+    private Vector3 targetFollowOffset;
+
+
+    private void Start()
+    {
+        cinemachineFollow = cinemachineCamera.GetComponent<CinemachineFollow>();
+        targetFollowOffset = cinemachineFollow.FollowOffset;
+    }
+    private void Update()
+    {
+        // Camera movement
+        HandleMovement();
+
+        // Camera rotation
+        HandleRotation();
+
+        // Camera zoom
+        HandleZoom();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 inputMoveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMoveDir.z = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputMoveDir.z = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMoveDir.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveDir.x = +1f;
+        }
+
+        float moveSpeed = 10f;
+        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 rotationVector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationVector.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationVector.y = -1f;
+        }
+
+        float rotationSpeed = 100f;
+        transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom()
+    {
+        float zoomAmmount = 1f;
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            targetFollowOffset.y -= zoomAmmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFollowOffset.y += zoomAmmount;
+        }
+
+        // Clamp values to stay within MIN and MAX zoom
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+
+        // Ease the zooming
+        float zoomSpeed = 20f;
+        cinemachineFollow.FollowOffset = Vector3.Lerp(cinemachineFollow.FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Testubg : MonoBehaviour
+{
+
+
+
+    
+    
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
