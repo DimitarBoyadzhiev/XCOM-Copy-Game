8f16bc623a144b062dc5b1bdeb3e9790d779b0f8
Dimitar Boyadzhiev <dimitar.boyadzhiev02@gmail.com>
2025-09-14 11:29:17 +0300

    feat: Base Enemy AI

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,7 +7,8 @@ public class Unit : MonoBehaviour
     private const int ACTION_POINTS_MAX = 2;
 
     public static event EventHandler OnAnyActionPointsChanged;
-
+    public static event EventHandler OnAnyUnitSpawned;
+    public static event EventHandler OnAnyUnitDead;
 
 
 
@@ -38,6 +39,8 @@ public class Unit : MonoBehaviour
         TurnSystem.Instance.OnTurnChanged += TurnSystem_OnTurnChanged;
 
         healthSystem.OnDead += HealthSystem_OnDead;
+
+        OnAnyUnitSpawned?.Invoke(this, EventArgs.Empty);
     }
 
     private void Update()
@@ -140,5 +143,7 @@ public class Unit : MonoBehaviour
         LevelGrid.Instance.RemoveUnitAtGridPosition(gridPosition, this);
 
         Destroy(gameObject);
+
+        OnAnyUnitDead?.Invoke(this, EventArgs.Empty);
     }
 }
