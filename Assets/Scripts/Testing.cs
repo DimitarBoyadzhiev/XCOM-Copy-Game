540b26336911c2c6590df10d4e6d539344c01bc6
Dimitar Boyadzhiev <dimitar.boyadzhiev02@gmail.com>
2025-09-16 12:38:25 +0300

    feat: Implemented A* pathfinding

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,6 +1,8 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Testubg : MonoBehaviour
+public class Testing : MonoBehaviour
 {
 
     [SerializeField] private Unit unit;
@@ -12,6 +14,22 @@ public class Testubg : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
+            GridPosition startGridPosition = new GridPosition(0, 0);
+
+            List<GridPosition> gridPositionList = Pathfinding.Instance.FindPath(startGridPosition, mouseGridPosition);
+            
+            for (int i = 0; i < gridPositionList.Count - 1; i++)
+            {
+                Debug.DrawLine(
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i]),
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]),
+                    Color.white,
+                    10f
+                    );
+            }
+        }
     }
 }
