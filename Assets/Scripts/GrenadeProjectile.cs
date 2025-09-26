f216ea5e96b031a84277f6d11600084bba7d9e1a
Dimitar Boyadzhiev <dimitar.boyadzhiev02@gmail.com>
2025-09-26 15:36:00 +0300

    feat: Destructable crate and fixes to grid update

--- Assets/Scripts/GrenadeProjectile.cs
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -41,6 +41,11 @@ public class GrenadeProjectile : MonoBehaviour
                {
                     targetUnit.Damage(30);
                }
+
+               if (collider.TryGetComponent<DestructableCrate>(out DestructableCrate destructableCrate))
+               {
+                   destructableCrate.Damage();
+               }
             }
 
             OnAnyGrenadeExploded?.Invoke(this, EventArgs.Empty);
